eae815a1f857c2b3aa9daaa335e37e09cc6201f3
Brasen Xu <brasenxu@gmail.com>
2022-12-22 15:03:20 -0500

    Initial commit (hopefully this works fingers crossed)

---
+++ main.py
@@ -0,0 +1,28 @@
+# Ponytails >>>
+
+import discord
+import hidden
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='#', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game("Archiving NOW!"))
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Here is the ping: {round(bot.latency * 1000)} ms')
+
+
+@bot.command()
+async def repeat(ctx, str):
+    await ctx.send(str)
+
+bot.run(hidden.key)
