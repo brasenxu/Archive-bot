eb4c5f25cfc9ed13b57173a269dca0458ee94000
Brasen Xu <brasenxu@gmail.com>
2022-12-23 16:20:02 -0500

    added a separate command for only messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Ponytails >>>
+# Girls with ponytails are so hot
 import pandas as pd
 import discord
 import hidden
@@ -22,18 +22,9 @@ async def ping(ctx):
 
 
 @bot.command()
-async def messages(ctx):
-    counter = 0
-    async for message in ctx.channel.history(limit=50):
-        if message.author == bot.user:
-            counter += 1
-    await ctx.send(counter)
+async def archive_all(ctx):
 
-
-@bot.command()
-async def archive(ctx):
-
-    messages_data = pd.DataFrame(columns=['content', 'attachment', 'author', 'time'])
+    messages_all_data = pd.DataFrame(columns=['content', 'attachment', 'author', 'time'])
 
     async for message in ctx.channel.history(limit=1000, oldest_first=True):
 
@@ -46,6 +37,22 @@ async def archive(ctx):
                              'attachment': message_attach,
                              'author': message.author.name,
                              'time': str(message.created_at)[:16]},
+                            index=[0])
+        messages_all_data = pd.concat([messages_all_data, data], ignore_index=True)
+
+    file = "messages_data.csv"
+    messages_all_data.to_csv(file)
+
+    await ctx.send("Here is your archive!", file=discord.File(file))
+
+
+@bot.command()
+async def archive_messages(ctx):
+    messages_data = pd.DataFrame(columns=['messages'])
+
+    async for message in ctx.channel.history(limit=1000, oldest_first=True):
+
+        data = pd.DataFrame({'messages': message.content},
                             index=[0])
         messages_data = pd.concat([messages_data, data], ignore_index=True)
 
