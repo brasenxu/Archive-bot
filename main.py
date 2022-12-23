df3c2b0c4c300f5299a79e532ba473940fdb3f86
Brasen Xu <brasenxu@gmail.com>
2022-12-23 14:11:07 -0500

    changed the prefix from '#' to '*'

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='#', intents=intents)
+bot = commands.Bot(command_prefix='*', intents=intents)
 
 
 @bot.event
